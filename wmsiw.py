6ec0d09af78d6ef3213145921b3a3e74bc84869e
dsheng <daniellsheng@gmail.com>
2019-07-13 00:23:30 -0700

    added poster image to random page

--- wmsiw.py
+++ wmsiw.py
@@ -22,17 +22,21 @@ def randomMovie():
     randID = getrandomID()
     response = requests.get(url = endpoint + randID + apikey)
     data = response.json()
+  
     print(data)
-    print(data['Title'])
-    return render_template("index.html")       
+    if data['Response'] == 'False':
+        randomMovie()
+    title = data['Title']
+    year = data['Year']
+    genre = data['Genre']
+    director = data['Director']
+    poster = data['Poster']
+    rating = data['imdbRating']
+
+    return render_template("movie.html", title = title, year = year, genre = genre, director = director, poster = poster, rating = rating)       
 
 @app.route("/")
 def hello():
-#     randID = getrandomID()
-#     response = requests.get(url = endpoint + randID + apikey)
-#     data = response.json()
-#     print(data)
-#     print(data['Title'])
     return render_template("index.html")
 
 if __name__ == "__main__":
